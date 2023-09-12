194a26f8e90a1e562f639529a4edd2df0d9e8e6f
Wahyu Adi Nugroho <68644404+wahyuadin@users.noreply.github.com>
2023-09-12 13:35:23 +0700

    Update api-cuaca.html

--- api-cuaca.php
+++ api-cuaca.php
@@ -1,18 +1,71 @@
-<?php
-// URL data XML dari BMKG
-$xmlUrl = 'https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-JawaBarat.xml';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Konversi XML ke JSON</title>
+</head>
+<body>
+    <h1>Data BMKG Jawa Barat</h1>
+    <pre id="jsonOutput"></pre>
 
-// Mengambil data XML dari URL
-$xmlString = file_get_contents($xmlUrl);
+    <script>
+        var xmlUrl = 'https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-JawaBarat.xml';
 
-// Memuat data XML ke dalam objek SimpleXML
-$xml = simplexml_load_string($xmlString);
+        // Menggunakan objek XMLHttpRequest untuk mengambil data XML
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', xmlUrl, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var xmlString = xhr.responseText;
 
-// Mengonversi SimpleXML ke dalam format array
-$json = json_encode($xml);
-$array = json_decode($json, true);
+                // Memuat data XML ke dalam objek DOMParser
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(xmlString, 'text/xml');
 
-header('Content-Type: application/json');
-// Menampilkan data array
-echo($json);
-?>
+                // Mengonversi objek DOM menjadi JSON
+                var json = xmlToJson(xmlDoc);
+
+                // Menampilkan data JSON
+                var jsonOutput = document.getElementById('jsonOutput');
+                jsonOutput.textContent = JSON.stringify(json, null, 2);
+                jsonOutput.setAttribute('content-type', 'application/json');
+            }
+        };
+        xhr.send();
+
+        // Fungsi untuk mengonversi objek DOM menjadi JSON
+        function xmlToJson(xml) {
+            var obj = {};
+
+            if (xml.nodeType === 1) {
+                if (xml.attributes.length > 0) {
+                    obj['@attributes'] = {};
+                    for (var j = 0; j < xml.attributes.length; j++) {
+                        var attribute = xml.attributes[j];
+                        obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+                    }
+                }
+            } else if (xml.nodeType === 3) {
+                obj = xml.nodeValue;
+            }
+
+            if (xml.hasChildNodes()) {
+                for (var i = 0; i < xml.childNodes.length; i++) {
+                    var item = xml.childNodes[i];
+                    var nodeName = item.nodeName;
+                    if (typeof obj[nodeName] == 'undefined') {
+                        obj[nodeName] = xmlToJson(item);
+                    } else {
+                        if (typeof obj[nodeName].push == 'undefined') {
+                            var old = obj[nodeName];
+                            obj[nodeName] = [];
+                            obj[nodeName].push(old);
+                        }
+                        obj[nodeName].push(xmlToJson(item));
+                    }
+                }
+            }
+            return obj;
+        }
+    </script>
+</body>
+</html>
